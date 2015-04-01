84c3945d444a0814f5429e8e4439606bb0640e8d
Matías Fidemraizer <fidemraizer@hotmail.com>
2015-04-01 15:56:44 +0200

    Changes [AssemblyCulture("en-US")] to [AssemblyCulture("")]

--- StackRedis.AspNet.Identity/Properties/AssemblyInfo.cs
+++ StackRedis.AspNet.Identity/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("StackRedis.AspNet.Identity")]
 [assembly: AssemblyCopyright("Copyright © Matías Fidemraizer 2015")]
 [assembly: AssemblyTrademark("Matías Fidemraizer")]
-[assembly: AssemblyCulture("en-US")]
+[assembly: AssemblyCulture("")]
 
 // Si establece ComVisible como false, los tipos de este ensamblado no estarán visibles 
 // para los componentes COM.  Si necesita obtener acceso a un tipo de este ensamblado desde 
@@ -49,5 +49,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o establecer como predeterminados los números de compilación y de revisión 
 // mediante el carácter '*', como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
